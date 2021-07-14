7bd07fbca5f1d99afb8ea23f31704e19e8788160
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2021-07-14 16:13:03 +0700

    Delete app directory

--- app/config/jt.js
+++
@@ -1,6 +0,0 @@
-var _config = {
-    'environment': 'dev', // [dev, live]
-    'baseURL'    : '',
-    'jtURL'      : 'https://jalantikus.com/',
-    'jtAPI'      : 'https://api.jalantikus.com/',
-};
