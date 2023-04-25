251b71b047debebf2efe9de2f4db387b935aca08
taqin <abdulmuttaqin456@gmail.com>
2023-04-25 07:19:24 +0700

    Create random.js

---
+++ random.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const axios = require('axios');
+
+async function getRandomAyah() {
+  const apiUrl = 'https://api.alquran.cloud/v1/quran/en.asad';
+  const response = await axios.get(apiUrl);
+  const ayahs = response.data.data.ayahs;
+  const randomAyah = ayahs[Math.floor(Math.random() * ayahs.length)];
+  return randomAyah;
+}
+
+
+async function updateReadme(ayah) {
+  const readmePath = './README.md';
+  const readmeContent = fs.readFileSync(readmePath, 'utf-8');
+  const regex = /<!--START_SECTION:quran-->[\s\S]*<!--END_SECTION:quran-->/;
+
+  let updatedAyah = `<!--START_SECTION:quran-->\n\n`;
+  updatedAyah += `**Surah ${ayah.surah.number}: ${ayah.surah.name} (${ayah.surah.englishName})**\n\n`;
+  updatedAyah += `${ayah.numberInSurah}. ${ayah.text} - ${ayah.translation.en}\n`;
+  updatedAyah += `<!--END_SECTION:quran-->`;
+
+  const updatedReadmeContent = readmeContent.replace(regex, updatedAyah);
+  fs.writeFileSync(readmePath, updatedReadmeContent);
+}
+
+async function main() {
+  const randomAyah = await getRandomAyah();
+  await updateReadme(randomAyah);
+}
+
+main();
