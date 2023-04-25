6e41b8f376ed580488e5624f39ae439aed13f3a9
taqin <abdulmuttaqin456@gmail.com>
2023-04-25 07:40:31 +0700

    Update random.js

--- random.js
+++ random.js
@@ -4,13 +4,21 @@ const axios = require('axios');
 async function getRandomAyah() {
   const apiUrl = 'https://api.alquran.cloud/v1/quran/en.asad';
   const response = await axios.get(apiUrl);
-
+// console.log(response.data.data);
   if (response.data.status === 'OK' && response.data.data) {
-    const surahlength = response.data.data.surah;
-    const randomsurah = response.data.data.surah[Math.floor(Math.random() * surahlength.length)];
+    const surahlength = response.data.data.surahs;
+    const randomsurah = response.data.data.surahs[Math.floor(Math.random() * surahlength.length)];
     const ayahlength = randomsurah.ayahs;
-    const randomAyah = ayahlength[Math.floor(Math.random() * surahlength.ayahlength)].text;
-    return randomAyah;
+ 
+    const randomAyah = ayahlength[Math.floor(Math.random() * ayahlength.length)];
+    console.log(randomAyah);
+    return {
+        surah : randomsurah.name,
+        englishName : randomsurah.englishName,
+        surahnumber : randomsurah.number,
+        ayah : randomAyah.text,
+        number : randomAyah.numberInSurah,
+    };
   } else {
     throw new Error('Unable to fetch Ayahs from the API.');
   }
@@ -24,8 +32,8 @@ async function updateReadme(ayah) {
   const regex = /<!--START_SECTION:quran-->[\s\S]*<!--END_SECTION:quran-->/;
 
   let updatedAyah = `<!--START_SECTION:quran-->\n\n`;
-  updatedAyah += `**Surah ${ayah.surah.number}: ${ayah.surah.name} (${ayah.surah.englishName})**\n\n`;
-  updatedAyah += `${ayah.numberInSurah}. ${ayah.text} - ${ayah.translation.en}\n`;
+  updatedAyah += `**Surah ${ayah.number}: ${ayah.surah} (${ayah.englishName})**\n\n`;
+  updatedAyah += `${ayah.ayah}\n`;
   updatedAyah += `<!--END_SECTION:quran-->`;
 
   const updatedReadmeContent = readmeContent.replace(regex, updatedAyah);
